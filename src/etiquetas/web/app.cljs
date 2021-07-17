96a473689b252d54a957cdc6f92a9304b9d032b4
William Calderipe <wcalderipe@gmail.com>
2021-07-17 11:33:07 +0200

    Initial commit with shadow-cljs, reagent, and re-frame

---
+++ src/etiquetas/web/app.cljs
@@ -0,0 +1,18 @@
+(ns etiquetas.web.app
+  (:require [re-frame.core :as rf]
+            [reagent.core :as r]
+            [reagent.dom]))
+
+(defn mount-root
+  []
+  (reagent.dom/render [:div [:h1 "Hello"]]
+                      (js/document.getElementById "main")))
+
+(defn reload
+  []
+  (rf/clear-subscription-cache!)
+  (mount-root))
+
+(defn main
+  []
+  (mount-root))
