6f0dcdcc409058396ec7ea3d5bc944ef6ef0f7a7
William Calderipe <wcalderipe@gmail.com>
2021-07-20 08:24:25 +0200

    Prototype with clickable custom markers on the map

--- src/etiquetas/web/app.cljs
+++ src/etiquetas/web/app.cljs
@@ -1,11 +1,41 @@
 (ns etiquetas.web.app
-  (:require [re-frame.core :as rf]
+  (:require ["google-map-react" :as GoogleMap]
+            [re-frame.core :as rf]
             [reagent.core :as r]
             [reagent.dom]))
 
+(def paris-pos [48.8566 2.3522])
+(def louvre-pos [48.8606 2.3376])
+(def hotel-de-ville-pos [48.8566 2.3522])
+
+(defn pos->map
+  [[lat lng]]
+  {:lat lat :lng lng})
+
+(defn simple-map
+  []
+  [:div {:style {:width "100vh" :height "100%"}}
+   [:> GoogleMap {:zoom             11
+                  :defaultCenter    (pos->map paris-pos)
+                  ;; For some reason, if I change this key name format to
+                  ;; kebab-case, it doesn't work.
+                  :bootstrapURLKeys {:key ""}
+                  :on-child-click (fn [key props]
+                                    (js/console.log "onChildClick" key props))}
+
+    ;; 1. `lat` and `lng` props must be at the parent marker; otherwise, the it
+    ;; doesn't stick on the map when scrolling and zooming.
+    ;; 2. Props are passed to `:on-child-click` handler as a JavaScript object.
+    [:div (merge (pos->map hotel-de-ville-pos)
+                 {:slug "hotel-de-ville"})
+     "Hotel de Ville"]
+
+    [:div (pos->map louvre-pos)
+     "Louvre Museum"]]])
+
 (defn mount-root
   []
-  (reagent.dom/render [:div [:h1 "Hello"]]
+  (reagent.dom/render [simple-map]
                       (js/document.getElementById "main")))
 
 (defn reload
